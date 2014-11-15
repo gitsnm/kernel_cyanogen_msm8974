2868538ffa689ad49894edf24588d1a99e7395fd
anarkia1976 <stefano.villa1976@gmail.com>
2014-11-16 00:04:29 +0100

    cpufreq: interactive: make hispeed_freq attribute writeable also for groups

--- drivers/cpufreq/cpufreq_interactive.c
+++ drivers/cpufreq/cpufreq_interactive.c
@@ -896,7 +896,7 @@ static ssize_t store_hispeed_freq(struct kobject *kobj,
 	return count;
 }
 
-static struct global_attr hispeed_freq_attr = __ATTR(hispeed_freq, 0644,
+static struct global_attr hispeed_freq_attr = __ATTR(hispeed_freq, 0664,
 		show_hispeed_freq, store_hispeed_freq);
 
 static ssize_t show_sampling_down_factor(struct kobject *kobj,
